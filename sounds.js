f59a7b56afa4332e2b9694d8779c8ffaa8bedfe7
izzymo34 <81452878+izzymo34@users.noreply.github.com>
2022-03-27 04:49:09 -0500

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,9 +1,17 @@
 var audio1 = document.getElementById('audio1');
 var audio2 = document.getElementById('audio2');
 var audio3 = document.getElementById('audio3');
+var audio4 = document.getElementById('audio4');
+var audio5 = document.getElementById('audio5');
+var audio6 = document.getElementById('audio6');
+var audio7 = document.getElementById('audio7');
 var playPauseBTN1 = document.getElementById('playPauseBTN1');
 var playPauseBTN2 = document.getElementById('playPauseBTN2');
 var playPauseBTN3 = document.getElementById('playPauseBTN3');
+var playPauseBTN4 = document.getElementById('playPauseBTN4');
+var playPauseBTN5 = document.getElementById('playPauseBTN5');
+var playPauseBTN6 = document.getElementById('playPauseBTN6');
+var playPauseBTN7 = document.getElementById('playPauseBTN7');
 var count = 0;
 
 function playPause1(){
@@ -61,4 +69,80 @@ function stop3(){
 	audio3.pause();
 	audio3.currentTime = 0;
 	playPauseBTN3.innerHTML = "Play &#9658;";
+}
+
+function playPause4(){
+	if(count == 0){
+		count = 1;
+		audio4.play();
+		playPauseBTN4.innerHTML = "Pause &#9208;";
+	} else{
+		count = 0;
+		audio4.pause();
+		playPauseBTN4.innerHTML = "Play &#9658;";
+	}
+}
+
+function stop4(){
+	playPause4();
+	audio4.pause();
+	audio4.currentTime = 0;
+	playPauseBTN4.innerHTML = "Play &#9658;";
+}
+
+function playPause5(){
+	if(count == 0){
+		count = 1;
+		audio5.play();
+		playPauseBTN5.innerHTML = "Pause &#9208;";
+	} else{
+		count = 0;
+		audio5.pause();
+		playPauseBTN5.innerHTML = "Play &#9658;";
+	}
+}
+
+function stop5(){
+	playPause5();
+	audio5.pause();
+	audio5.currentTime = 0;
+	playPauseBTN5.innerHTML = "Play &#9658;";
+}
+
+function playPause6(){
+	if(count == 0){
+		count = 1;
+		audio6.play();
+		playPauseBTN6.innerHTML = "Pause &#9208;";
+	} else{
+		count = 0;
+		audio6.pause();
+		playPauseBTN6.innerHTML = "Play &#9658;";
+	}
+}
+
+function stop6(){
+	playPause6();
+	audio6.pause();
+	audio6.currentTime = 0;
+	playPauseBTN6.innerHTML = "Play &#9658;";
+}
+
+function playPause7(){
+	if(count == 0){
+		count = 1;
+		audio7.play();
+		playPauseBTN7.innerHTML = "Pause &#9208;";
+	} else{
+		count = 0;
+		audio7.pause();
+		playPauseBTN7.innerHTML = "Play &#9658;";
+	}
+}
+
+function stop7(){
+	playPause7();
+	audio7.pause();
+	audio7.currentTime = 0;
+	playPauseBTN7.innerHTML = "Play &#9658;";
 }
